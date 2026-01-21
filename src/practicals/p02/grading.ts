2c55c1ab93ee880ea90bfd91a5afbfea904ddb25
folk23444 <Thanyaton.jv@gmail.com>
2026-01-21 14:48:26 +0700

    test

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,21 +1,17 @@
 // Write your code below
-const grade = Number(process.argv[2]);
+const score = Number(process.argv[2]);
 
-if (grade < 0 || grade > 100)
-    {
-      console.log("Invalid input");
-    }else if (80 >= grade) {
-  console.log("A");
-} else if (79 >= grade) 
-{
-  console.log("B");
-} else if (69 >= grade) 
-{
-  console.log("C");
-} else if (59 >= grade) 
-{
-  console.log("D");
-} else if (49 >= grade) 
-{
-  console.log("F");
+if (score < 0 || score > 100) {
+    console.log("Invalid input");
+}
+if (score >= 80) {
+console.log("A");
+} else if (score >= 79) {
+console.log("B");
+} else if (score >= 69) {
+console.log("C");
+} else if (score >= 59) {
+console.log("D");
+}else if (score >= 49) {
+console.log("F");
 }
