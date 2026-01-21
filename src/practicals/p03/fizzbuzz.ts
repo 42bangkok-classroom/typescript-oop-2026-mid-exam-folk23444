6e5a40f8ac89fa3ac2cf3a37600e6e90c6f9a396
folk23444 <Thanyaton.jv@gmail.com>
2026-01-21 15:09:07 +0700

    test

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,7 +1,7 @@
 // Write your code below
 const num = Number(process.argv[2]);
  if (num <= 0){
-    process.Exit[2]
+    console.log("")
  }
 else if (num % 3 === 0 && num % 5 === 0 ){
     console.log("FizzBuzz")
