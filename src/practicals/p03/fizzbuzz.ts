0e084b073cfbc44033e7a813a4a715895bfc37e8
folk23444 <Thanyaton.jv@gmail.com>
2026-01-21 15:23:41 +0700

    test

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,9 +1,8 @@
 // Write your code below
 const num = Number(process.argv[2]);
- if (num <= 0){
+if (num <= 0){
     console.log("")
- }
-else if (num % 3 === 0 && num % 5 === 0 ){
+}else if (num % 3 === 0 && num % 5 === 0 ){
     console.log("FizzBuzz")
 }else if (num % 3 === 0){
     console.log("Fizz")
