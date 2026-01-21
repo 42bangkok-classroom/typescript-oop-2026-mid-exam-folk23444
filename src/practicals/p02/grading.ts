c08a2fae56e4a7bb0b38ab532fbcfc13f0c8e55f
folk23444 <Thanyaton.jv@gmail.com>
2026-01-21 15:07:25 +0700

    test

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -6,11 +6,11 @@ if (score < 0 || score > 100) {
 }
 if (score >= 80) {
 console.log("Grade is A");
-} else if (score >= 79) {
+} else if (score >= 79 || score >=70) {
 console.log("Grade is B");
-} else if (score >= 69) {
+} else if (score >= 69 || score >=60) {
 console.log("Grade is C");
-} else if (score >= 59) {
+} else if (score >= 59 || score >=50) {
 console.log("Grade is D");
 }else if (score >= 49) {
 console.log("Grade is F");
