16d614f09db140c9e247fa2971e3e44491f4dccc
folk23444 <Thanyaton.jv@gmail.com>
2026-01-21 16:00:22 +0700

    test

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -2,7 +2,7 @@
 const size = Number(process.argv[2]);
 for (let i = 1; i <= size; i++) {
     let line = "";
-    for (let j = 5; j >= 1; j--) {
+    for (let j = size; j >= 1; j--) {
         if (j <= i) {
             line += j.toString();
         } else {
